a62aff24f01833a3091a85ccfcb49838550401fa
minkyeong <mgk04222@naver.com>
2023-04-28 21:22:56 +0900

    [CREATE] #BTS-39 : 소설 디테일 페이지 footer component
    - 소설 디테일 페이지 footer component 생성

---
+++ components/layouts/DetailFooter.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import style from "@/components/layouts/DetailFooter.module.css";
+import Image from "next/image";
+export default function DetailFooter() {
+  return (
+    <div className={style.detailFooter}>
+      <div className={style.novelBuyBtn}>
+        <Image
+          src="/assets/images/icons/bookmark-2.svg"
+          alt="logo"
+          width={40}
+          height={40}
+          priority
+        />
+      </div>
+      <div className={style.novelReadBtn}>
+        <Image
+          src="/assets/images/icons/bookwhite.svg"
+          alt="logo"
+          width={40}
+          height={40}
+          priority
+        />
+        <div>무료로 첫편보기</div>
+      </div>
+    </div>
+  );
+}
