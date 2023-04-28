d28527d5b1883d24baeebf7fbe36fa5fb71e9b45
minkyeong <mgk04222@naver.com>
2023-04-28 21:28:01 +0900

    [CREATE] #BTS-39 : 소설 디테일 페이지 TagUi component
    - 소설 디테일 페이지 TagUi component 생성

---
+++ components/ui/TagUi.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from '@/components/ui/TagUi.module.css'
+export default function TagUi(props:{title:string}) {
+  return (
+    <div className={style.tags}>
+      <div className={style.tagtitle}><p>{props.title}</p></div>
+    </div>
+  )
+}
