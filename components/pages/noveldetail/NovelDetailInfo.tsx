f050fa8475016e53bc09bf1ea362eee6c5c516df
minkyeong <mgk04222@naver.com>
2023-04-29 14:26:26 +0900

    [UPDATE] #BTS-39 : 디테일 페이지 CountViewUI, NovelCard 컴포넌트 수정
    - UI flex-direction 추가
    - backgroundwidth,height 추가
    - view, starRating 추가

--- components/pages/noveldetail/NovelDetailInfo.tsx
+++ components/pages/noveldetail/NovelDetailInfo.tsx
@@ -3,29 +3,42 @@ import Image from "next/image";
 import style from "@/components/pages/noveldetail/NovelDetailinfo.module.css";
 import CountViewUi from "@/components/ui/CountViewUi";
 import NovelCard from "@/components/ui/NovelCard";
-export default function NovelDetailInfo() {
+export default function NovelDetailInfo(props: {
+  title: string;
+  description: string;
+  serializationStatus: string;
+  genre: string;
+  thumbnail: string;
+  views: number;
+  starRating: number;
+}) {
   return (
     <div className={style.novelMainInfo}>
-      <NovelCard width={250} height={250}/>
+      <NovelCard width={200} height={200} backgroundwidth={150} backgroundheight={150} thumbnail={props.thumbnail} />
       <div className={style.detailinfo}>
-        <p>신과 함께 레벨업</p>
-        <p>흑아인 | 완결 | 웹소설판타지</p>
+        <p>{props.title}</p>
+        <p>
+          {props.description} | {props.serializationStatus} |{props.genre}
+        </p>
       </div>
       <div className={style.detailnovellikes}>
         <CountViewUi
           icon="/assets/images/icons/eye.svg"
-          count={1332}
+          count={props.views}
           color="black"
+          flexDirection="column"
         />
         <CountViewUi
           icon="/assets/images/icons/star.svg"
-          count={9.5}
+          count={props.starRating}
           color="black"
+          flexDirection="column"
         />
         <CountViewUi
           icon="/assets/images/icons/list.svg"
           count={355}
           color="black"
+          flexDirection="column"
         />
       </div>
     </div>
