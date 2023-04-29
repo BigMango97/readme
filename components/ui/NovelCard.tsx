18b41434e9ce184fb4fa5fa03765dcb254d0587c
minkyeong <mgk04222@naver.com>
2023-04-29 15:50:43 +0900

    [UPDATE] #BTS-39 : 소설 카드 컴포넌트 수정
    - background width, height
    - props

--- components/ui/NovelCard.tsx
+++ components/ui/NovelCard.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import style from "@/components/ui/NovelCard.module.css";
 import Image from "next/image";
-export default function NovelCard(props:{width:number,height:number}) {
-  return ( 
-    <div className={style.detailImgWrap} style={{width:props.width,height:props.height}} >
+export default function NovelCard(props: {
+  width: number;
+  height: number;
+  backgroundwidth:number;
+  backgroundheight:number;
+  thumbnail: string;
+}) {
+  return (
+    <div
+      className={style.detailImgWrap}
+      style={{ width: props.backgroundwidth, height: props.backgroundheight }}
+    >
       <Image
-        src="/assets/images/dummy/bestItem01.png"
-        alt="dsddd"
+        src={props.thumbnail}
+        alt="썸네일 이미지"
         width={props.width}
         height={props.height}
       />
