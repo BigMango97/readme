61475a7ceaf37ef9e081a2768a2d94a98d1de016
minkyeong <mgk04222@naver.com>
2023-04-30 17:03:05 +0900

    [UPDATE] #BTS-39 : EpisodeInfo 컴포넌트내의 NovelCard 컴포넌트 삭제
    - Episode 컴포넌트를 만들기위해 NovelCard 컴포넌트 삭제

--- components/pages/noveldetail/EpisodeInfo.tsx
+++ components/pages/noveldetail/EpisodeInfo.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import NovelCard from "@/components/ui/NovelCardImg";
 import style from "@/components/pages/noveldetail/EpisodeInfo.module.css";
-export default function EpisodeInfo(props:{thumbnail:string}) {
+import Image from "next/image";
+export default function EpisodeInfo() {
   return (
     <div className={style.container}>
+
       <div className={style.episodeCard}>
-        <NovelCard width={100} height={100} backgroundwidth={90} backgroundheight={90} backgroundColor={"rgb(110,72,235)"} thumbnail={props.thumbnail} />
+        <div className={style.episodeCardImageInfo}>
+          <Image
+            src={"/assets/images/dummy/bestItem01.png"}
+            alt="썸네일 이미지"
+            width={100}
+            height={100}
+          />
+        </div>
         <div className={style.episodeCardInfo}>
           <div>{"흑아인"}</div>
           <div>{"2025-03-12"}</div>
@@ -13,8 +21,16 @@ export default function EpisodeInfo(props:{thumbnail:string}) {
           <div>{"무료"}</div>
         </div>
       </div>
+
       <div className={style.episodeCard}>
-        <NovelCard width={100} height={100} backgroundwidth={90} backgroundheight={90} backgroundColor={"rgb(110,72,235)"} thumbnail={props.thumbnail} />
+        <div className={style.episodeCardImageInfo}>
+          <Image
+            src={"/assets/images/dummy/bestItem01.png"}
+            alt="썸네일 이미지"
+            width={100}
+            height={100}
+          />
+        </div>
         <div className={style.episodeCardInfo}>
           <div>{"흑아인"}</div>
           <div>{"2025-03-12"}</div>
@@ -22,8 +38,16 @@ export default function EpisodeInfo(props:{thumbnail:string}) {
           <div>{"무료"}</div>
         </div>
       </div>
+
       <div className={style.episodeCard}>
-        <NovelCard width={100} height={100} backgroundwidth={90} backgroundheight={90} backgroundColor={"rgb(110,72,235)"} thumbnail={props.thumbnail}/>
+        <div className={style.episodeCardImageInfo}>
+          <Image
+            src={"/assets/images/dummy/bestItem01.png"}
+            alt="썸네일 이미지"
+            width={100}
+            height={100}
+          />
+        </div>
         <div className={style.episodeCardInfo}>
           <div>{"흑아인"}</div>
           <div>{"2025-03-12"}</div>
@@ -31,24 +55,8 @@ export default function EpisodeInfo(props:{thumbnail:string}) {
           <div>{"무료"}</div>
         </div>
       </div>
-      <div className={style.episodeCard}>
-        <NovelCard width={100} height={100} backgroundwidth={90} backgroundheight={90} backgroundColor={"rgb(110,72,235)"} thumbnail={props.thumbnail}/>
-        <div className={style.episodeCardInfo}>
-          <div>{"흑아인"}</div>
-          <div>{"2025-03-12"}</div>
-          <div>{9.6}</div>
-          <div>{"무료"}</div>
-        </div>
-      </div>
-      <div className={style.episodeCard}>
-        <NovelCard width={100} height={100} backgroundwidth={90} backgroundheight={90} backgroundColor={"rgb(110,72,235)"} thumbnail={props.thumbnail}/>
-        <div className={style.episodeCardInfo}>
-          <div>{"흑아인"}</div>
-          <div>{"2025-03-12"}</div>
-          <div>{9.6}</div>
-          <div>{"무료"}</div>
-        </div>
-      </div>
+
+  
     </div>
   );
 }
