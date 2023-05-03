16f396875ec898083bb4817589778637707c7def
sihyun3109 <sihyun3109@gmail.com>
2023-05-03 09:51:34 +0900

    [UPDATE]: 내 서재 CSS 수정 - 내 서재에 탭 메뉴 추가 및 수정 - 최근 탭 부분 퍼블리싱 추가

--- pages/library.tsx
+++ pages/library.tsx
@@ -1,16 +1,20 @@
-import Header from '@/components/layouts/Header'
-import MyBooks from '@/components/pages/library/MyBooks'
-import LibraryTop from '@/components/pages/library/LibraryTop'
-import React from 'react'
+import { NextPageWithLayout } from "./_app";
+import MyBooks from "@/components/pages/library/MyBooks";
+import LibraryTop from "@/components/pages/library/LibraryTop";
+import React from "react";
+import Layout from "@/components/layouts/layout";
 
-export default function Library() {
+const Library: NextPageWithLayout = () => {
   return (
     <>
-    <Header/>
-    <LibraryTop/>
-    <MyBooks/>
+      <LibraryTop />
+      <MyBooks />
     </>
-    
-    
-  )
-}
+  );
+};
+
+Library.getLayout = function getLayout(page: React.ReactNode) {
+  return <Layout>{page}</Layout>;
+};
+
+export default Library;
