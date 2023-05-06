f96d1af0ae3ed42784c1d4d792740f1d7f383414
minkyeong <mgk04222@naver.com>
2023-05-06 17:36:03 +0900

    [UPDATE] #BTS-39 : 소설상세페이지 변경
    - 태그 컴포넌트(넓이고정) : 전반적인 UI를 생각했을 때 한줄로 처리하는 것 갯수를 제한하는 것(3개)으로 협의
    - 구분선 컴포넌트(회색, 형광색) 구분
    - Title 컴포넌트 삭제 => 추후 필요시 컴포넌트화 할 예정
    - 에피소트 탭의 경우 무료인 소설, New소설로 구분이 되는데 높이에 영향을 미치지않게 똑같은 높이로 구현 -> 추후 컴포넌트화 진행예정
    - 댓글 탭도 마찬가지로 작성자와 작성자가 아닌경우로 구분이 되는데 높이에 영향을 미치지않게 똑같은 높이로 구현 -> 추후 컴포넌트화 진행예정

--- components/pages/noveldetail/CommentList.tsx
+++ components/pages/noveldetail/CommentList.tsx
@@ -2,104 +2,169 @@ import React from "react";
 import style from "@/components/pages/noveldetail/CommentList.module.css";
 import NewUi from "@/components/ui/NewUi";
 import Image from "next/image";
+import LineSeparator from "@/components/ui/LineSeparator";
 export default function CommentList() {
   return (
     <>
-      <div className={style.CommentCard}>
-        <div className={style.commentListInfo}>
-          <div className={style.commentListTitle}>
-            <div className={style.commentWriter}>밍개미</div>
-            <NewUi />
+      <div className={style.CommentContainer}>
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미</div>
+              <NewUi />
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
           </div>
-          <div className={style.commentDay}>2023.04.25</div>
-        </div>
-        <div className={style.comment}>
-          무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
-        </div>
-        <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
-      </div>
-
-      <div className={style.CommentCard}>
-        <div className={style.commentListInfo}>
-          <div className={style.commentListTitle}>
-            <div className={style.commentMyWrite}>Juliet(작성자)</div>
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
           </div>
-          <div className={style.commentDay}>2023.04.25</div>
-        </div>
-        <div className={style.comment}>
-          무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
-        </div>
-        <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
-        <div className={style.episodeIcons}>
-          <Image
-            src="/assets/images/icons/write.svg"
-            alt="left-arrow"
-            width={20}
-            height={20}
-            priority
-          />
-          <Image
-            src="/assets/images/icons/trash-2.svg"
-            alt="left-arrow"
-            width={20}
-            height={20}
-            priority
-          />
-        </div>
-      </div>
-
-      <div className={style.CommentCard}>
-        <div className={style.commentListInfo}>
-          <div className={style.commentListTitle}>
-            <div className={style.commentWriter}>밍개미</div>
-            <NewUi />
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
           </div>
-          <div className={style.commentDay}>2023.04.25</div>
         </div>
-        <div className={style.comment}>
-          무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
-        </div>
-        <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
-      </div>
-
-      <div className={style.CommentCard}>
-        <div className={style.commentListInfo}>
-          <div className={style.commentListTitle}>
-            <div className={style.commentWriter}>밍개미</div>
-            <NewUi />
+        <LineSeparator colorline="grayline" />
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미(작성자)</div>
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
           </div>
-          <div className={style.commentDay}>2023.04.25</div>
-        </div>
-        <div className={style.comment}>
-          무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
-        </div>
-        <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
-      </div>
-      <div className={style.CommentCard}>
-        <div className={style.commentListInfo}>
-          <div className={style.commentListTitle}>
-            <div className={style.commentWriter}>밍개미</div>
-            <NewUi />
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
           </div>
-          <div className={style.commentDay}>2023.04.25</div>
-        </div>
-        <div className={style.comment}>
-          무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
-        </div>
-        <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
-      </div>
-      <div className={style.CommentCard}>
-        <div className={style.commentListInfo}>
-          <div className={style.commentListTitle}>
-            <div className={style.commentWriter}>밍개미</div>
-            <NewUi />
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+            <div>
+              <Image
+                src="/assets/images/icons/write.svg"
+                alt="left-arrow"
+                width={20}
+                height={20}
+                priority
+              />
+              <Image
+                src="/assets/images/icons/trash-2.svg"
+                alt="left-arrow"
+                width={20}
+                height={20}
+                priority
+              />
+            </div>
           </div>
-          <div className={style.commentDay}>2023.04.25</div>
         </div>
-        <div className={style.comment}>
-          무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
+        <LineSeparator colorline="grayline" />
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미</div>
+              <NewUi />
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
+          </div>
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
+          </div>
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+          </div>
         </div>
-        <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+        <LineSeparator colorline="grayline" />
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미</div>
+              <NewUi />
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
+          </div>
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
+          </div>
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+          </div>
+        </div>
+        <LineSeparator colorline="grayline" />
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미</div>
+              <NewUi />
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
+          </div>
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
+          </div>
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+          </div>
+        </div>
+        <LineSeparator colorline="grayline" />
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미</div>
+              <NewUi />
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
+          </div>
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
+          </div>
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+          </div>
+        </div>
+        <LineSeparator colorline="grayline" />
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미</div>
+              <NewUi />
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
+          </div>
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
+          </div>
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+          </div>
+        </div>
+        <LineSeparator colorline="grayline" />
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미</div>
+              <NewUi />
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
+          </div>
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
+          </div>
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+          </div>
+        </div>
+        <LineSeparator colorline="grayline" />
+        <div className={style.CommentCard}>
+          <div className={style.commentListInfo}>
+            <div className={style.commentListTitle}>
+              <div className={style.commentWriter}>밍개미</div>
+              <NewUi />
+            </div>
+            <div className={style.commentDay}>2023.04.25</div>
+          </div>
+          <div className={style.comment}>
+            무협 좀비아포칼립스는 ㄹㅇ 빡센데 ㄷㄷ 화전민촌들이 다 휩쓸리겟네
+          </div>
+          <div className={style.episodeIcons}>
+            <div className={style.episodeTitle}>신과함께 레벨업 080화</div>
+          </div>
+        </div>
       </div>
     </>
   );
