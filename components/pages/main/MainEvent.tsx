ec45fc0e4dfe89c2bb84b0c313ccf6826d7a56b8
minkyeong <mgk04222@naver.com>
2023-05-07 13:35:17 +0900

    [UPDATE] #BTS-54 : 메인페이지 변경
    - 이벤트 아이템 디자인 변경
    - global css --readme-gray 색상 변경

--- components/pages/main/MainEvent.tsx
+++ components/pages/main/MainEvent.tsx
@@ -9,18 +9,17 @@ export default function MainEvent(props: { thumbnail: string }) {
           <h2>Event</h2>
           <div className={style.cardContainerEventTitle}>
             가슴이 웅장해지는
-            <br /> 따끈따끈한
-            <br /> 무료 소설
+            <br /> <strong>따끈따끈한</strong> 무료 소설
           </div>
-          <div className={style.cardContainerTitle}>사라진 비서에게</div>
           <p>흑아인 | 완결 | 웹소설판타지</p>
+          <div className={style.cardContainerTitle}>사라진 비서에게</div>
         </div>
         <div className={style.cardContainerImgInfo}>
           <Image
             src={"/assets/images/dummy/bestItem01.png"}
             alt="썸네일 이미지"
-            width={180}
-            height={180}
+            width={150}
+            height={150}
           />
         </div>
       </div>
