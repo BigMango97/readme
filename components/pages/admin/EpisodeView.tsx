2ee021c47119afef8c8ac703b033d7322da968ca
sihyun3109 <sihyun3109@gmail.com>
2023-05-08 18:39:52 +0900

    [UPDATE] #BTS-85 : ADMIN 에피소드 상세 페이지 -ADMIN 에피소드 상세 페이지 INPUT에서 DESCRIPTION으로 디자인 수정

--- components/pages/admin/EpisodeView.tsx
+++ components/pages/admin/EpisodeView.tsx
@@ -1,14 +1,5 @@
 import { PlusOutlined } from "@ant-design/icons";
-import {
-  Button,
-  Checkbox,
-  DatePicker,
-  Form,
-  Input,
-  Select,
-  Upload,
-  Space,
-} from "antd";
+import { Button, Descriptions, Input, Space } from "antd";
 import React, { useState } from "react";
 import style from "@/components/pages/admin/EpisodeView.module.css";
 import dayjs from "dayjs";
@@ -39,47 +30,37 @@ export default function EpisodeView() {
   return (
     <>
       <div className={style.container}>
-        <Form
-          labelCol={{ span: 20 }}
-          wrapperCol={{ span: 14 }}
-          layout="vertical"
-          disabled={componentDisabled}
-          style={{ maxWidth: 2000 }}
-        >
-          <div className={style.horizontal}>
-            <Form.Item label="작품명" style={{ width: 500 }}>
-              <Input value={epiData.title} />
-            </Form.Item>
-            <div className={style.button}>
-              <Form.Item>
-                <Button type="primary" htmlType="submit" disabled={false}>
-                  뒤로가기
-                </Button>
-              </Form.Item>
-            </div>
-          </div>
-          <div className={style.horizontal}>
-            <Form.Item label="등록일" style={{ width: 500 }}>
-              <Input value={epiData.createDate} />
-            </Form.Item>
-            <Form.Item label="수정일" style={{ width: 500 }}>
-              <Input value={epiData.updateDate} />
-            </Form.Item>
-          </div>
-          <div className={style.horizontal}>
-            <Form.Item label="회차상태" style={{ width: 500 }}>
-              <Input value={epiData.status} />
-            </Form.Item>
-            <Form.Item label="무료/유료" style={{ width: 500 }}>
-              <Input value={epiData.free} />
-            </Form.Item>
-          </div>
-          <div className={style.normal}>
-            <Form.Item label="컨텐츠 (소설내용)" style={{ width: 1800 }}>
-              <TextArea rows={14} value={epiData.content} />
-            </Form.Item>
-          </div>
-        </Form>
+        <Button type="primary" htmlType="submit" disabled={false}>
+          뒤로가기
+        </Button>
+        <div className={style.info}>
+          <Descriptions title="에피소드 정보">
+            <Descriptions.Item label="작품명">
+              {epiData.title}
+            </Descriptions.Item>
+          </Descriptions>
+          <Descriptions>
+            <Descriptions.Item label="등록일">
+              {epiData.createDate}
+            </Descriptions.Item>
+            <Descriptions.Item label="수정일">
+              {epiData.updateDate}
+            </Descriptions.Item>
+          </Descriptions>
+          <Descriptions>
+            <Descriptions.Item label="회차상태">
+              {epiData.status}
+            </Descriptions.Item>
+            <Descriptions.Item label="무료/유료">
+              {epiData.free}
+            </Descriptions.Item>
+          </Descriptions>
+          <Descriptions>
+            <Descriptions.Item label="컨텐츠(소설내용)">
+              {epiData.content}
+            </Descriptions.Item>
+          </Descriptions>
+        </div>
       </div>
     </>
   );
