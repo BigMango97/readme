117fb2ca66d1ed1ab921e71c04d03b2f66d7454a
minkyeong <mgk04222@naver.com>
2023-05-11 10:57:49 +0900

    [UPDATE] #BTS-00 : 도메인 추가
    - 이미지 도메인 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["btsimagebucket.s3.ap-northeast-2.amazonaws.com"],
+    domains: ["btsimagebucket.s3.ap-northeast-2.amazonaws.com","dn-img-page.kakao.com"],
   },
 }
 
