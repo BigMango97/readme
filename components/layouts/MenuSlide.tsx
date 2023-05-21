5b935a56d0b54802227265e12cfd737721bf613d
BigMango97 <mgk04222@naver.com>
2023-05-21 20:59:02 +0900

    [UPDATE] #BTS-120 : 모달 퍼블리싱
    - 로그인 되었을떄와 안되었을때 구분해서 퍼블 진행

--- components/layouts/MenuSlide.tsx
+++ components/layouts/MenuSlide.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import style from "@/components/layouts/MenuSlide.module.css";
 import Image from "next/image";
 import { useState } from "react";
-import { useEffect } from "react";
-import Link from "next/link";
+import SlideWebViewList from "../ui/SlideWebViewList";
 type Props = {
   onClose: () => void;
 };
@@ -29,6 +28,40 @@ export default function MenuSlide(props: Props) {
                 priority
               />
             </div>
+            {/**로그인 했을떄  */}
+            <div className={style.menuListInfo}>
+              <div className={style.mainInfoTitle}>
+                <p className={style.mainWelcomeTitle}>Welcome !</p>
+                <p className={style.subInfoTitle}>
+                  ReadMe에 오신것을 <br />
+                  환영합니다!
+                </p>
+                <div className={style.pointContainer}>
+                  <p>잔여 포인트 : 10,000p</p>
+                  <div className={style.pointBtn}>+충전</div>
+                </div>
+              </div>
+              <SlideWebViewList />
+            </div> 
+            {/**로그인 안했을떄 */}
+            {/* <div className={style.menuListInfo}>
+              <div className={style.mainTitle}>
+                <p>
+                  다양한 서비스를
+                  <br /> 이용하고 싶으시다면
+                  <br /> 로그인을 해주세요
+                </p>
+                <div className={style.kakaoLoginBtn}>
+                  <Image
+                    src="/assets/images/kakaoLogin.png"
+                    alt="kakaologin"
+                    width={250}
+                    height={50}
+                  ></Image>
+                </div>
+              </div>
+              <SlideWebViewList/>
+            </div> */}
           </div>
         </div>
       )}
