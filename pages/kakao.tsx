4b108e9c83bb6b40335e69dd9c512da4b5f2bba2
sihyun3109 <sihyun3109@gmail.com>
2023-05-22 20:48:49 +0900

    [UPDATE] #BTS-132 : 로그인 수정 -API URL 수정

--- pages/kakao.tsx
+++ pages/kakao.tsx
@@ -21,25 +21,27 @@ export default function Kakao() {
   console.log(code);
   useEffect(() => {
     if (code !== undefined) {
-      axios.get(`${baseUrl}/login/kakao?code=${code}`).then((res) => {
-        console.log(JSON.stringify(res.headers));
-        console.log("res.headers ", res.headers);
+      axios
+        .get(`https://api.readme.life/v1/user/login?code=${code}`)
+        .then((res) => {
+          console.log(JSON.stringify(res.headers));
+          console.log("res.headers ", res.headers);
 
-        // let myLogin = localStorage;
-        // myLogin.setItem("userId", res.data.data.userId);
-        // myLogin.setItem("refreshToken", res.headers.refreshtoken);
-        // myLogin.setItem("nickname", res.data.data.name);
+          // let myLogin = localStorage;
+          // myLogin.setItem("userId", res.data.data.userId);
+          // myLogin.setItem("refreshToken", res.headers.refreshtoken);
+          // myLogin.setItem("nickname", res.data.data.name);
 
-        // setLoginCheck(true);
-        // //api 콜 마다 헤더에 accessToken 담아 보내도록 설정
-        // axios.defaults.headers.common[
-        //   "Authorization"
-        // ] = `Bearer ${res.headers.accesstoken}`;
+          // setLoginCheck(true);
+          // //api 콜 마다 헤더에 accessToken 담아 보내도록 설정
+          // axios.defaults.headers.common[
+          //   "Authorization"
+          // ] = `Bearer ${res.headers.accesstoken}`;
 
-        // //refreshToken 쿠키에 저장
-        // setCookie("id", res.headers.refreshtoken, { path: "/" });
-        router.push("/mypage");
-      });
+          // //refreshToken 쿠키에 저장
+          // setCookie("id", res.headers.refreshtoken, { path: "/" });
+          router.push("/mypage");
+        });
     }
   }, [code]);
   // window.Kakao.API.request({
