dead8996a8c50f8bd2ef0bcd2186ad7b2d66347f
sihyun3109 <sihyun3109@gmail.com>
2023-05-24 20:25:17 +0900

    [UPDATE] #BTS-140 : 결제 수정 -코드 정리

--- pages/approval.tsx
+++ pages/approval.tsx
@@ -1,27 +1,25 @@
-import { payState } from "@/state/payState";
 import axios from "@/configs/axiosConfig";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
 
 export default function Approval() {
   const router = useRouter();
   const pg_token = router.query.pg_token;
-  //const payData = useRecoilValue(payState);
 
   console.log("pg_token1", pg_token);
   useEffect(() => {
     const tid = localStorage.getItem("tid");
+    const uuid = localStorage.getItem("uuid");
     const partnerOrderId = localStorage.getItem("partnerOrderId");
     if (pg_token !== undefined) {
-      console.log("pg_token2", pg_token);
-      console.log("payData.tid", tid);
-      console.log("payData.partnerOrderId", partnerOrderId);
+      // console.log("pg_token2", pg_token);
+      // console.log("payData.tid", tid);
+      // console.log("payData.partnerOrderId", partnerOrderId);
       axios
         .post(`/payments-service/v1/payments/approve`, {
           tid: tid,
           partnerOrderId: partnerOrderId,
-          uuid: "11",
+          uuid: "11", //uuid 수정
           pgToken: pg_token,
         })
         .then((res) => {
