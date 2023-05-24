d8e4c24e8259ce8b90174bfe898e850368d9e1d1
sihyun3109 <sihyun3109@gmail.com>
2023-05-24 17:14:18 +0900

    [UPDATE] #BTS-140 : 결제 수정 -백과 연결을 위한 수정.

--- components/pages/point/PointBottom.tsx
+++ components/pages/point/PointBottom.tsx
@@ -3,14 +3,34 @@ import style from "@/components/pages/point/PointBottom.module.css";
 import LineSeparator from "@/components/ui/LineSeparator";
 import { listAmountData } from "@/data/amountData";
 import axios from "@/configs/axiosConfig";
+import { useRecoilState } from "recoil";
+import { payState } from "@/state/payState";
 
 export default function pointBottom() {
   //const uuid = localStorage.getItem("uuid");
+  const [payData, setPayData] = useRecoilState(payState);
   const clickMoney = (point: number) => {
-    axios.post(`/payments-service/v1/payments/ready`, {
-      point: point,
-      uuid: "11",
-    });
+    axios
+      .post(`/payments-service/v1/payments/ready`, {
+        point: point,
+        uuid: "11",
+      })
+      .then((res) => {
+        console.log(res);
+        setPayData({
+          tid: res.data.data.tid,
+          partnerOrderId: res.data.data.partner_order_id,
+        });
+        window.open(res.data.data.next_redirect_pc_url);
+
+        // axios.post(`/payments-service/v1/payments/approve`, {
+        //   partner_order_id: res.data.data.partner_order_id,
+        //   tid: res.data.data.tid,
+        // });
+        // res.data.data
+        // partner_order_id
+        // tid
+      });
   };
   return (
     <>
--- pages/approval.tsx
+++ pages/approval.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { payState } from "@/state/payState";
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
 
 export default function approval() {
+  const router = useRouter();
+  const pg_token = router.query.pg_token;
+  const [payData, setPayData] = useRecoilState(payState);
+
+  console.log("pg_token", router.query.pg_token);
+  useEffect(() => {
+    if (pg_token !== undefined) {
+      console.log("pg_token", pg_token);
+      axios
+        .post(`/payments-service/v1/payments/approve`, {
+          tid: payData.tid,
+          partnerOrderId: payData.partnerOrderId,
+          uuid: "11",
+          pgToken: pg_token,
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    }
+  }, []);
+
   return <div>approval</div>;
 }
