28eecb5906f67fb9696d2a867ba9bcd33912e9af
sihyun3109 <sihyun3109@gmail.com>
2023-05-24 13:54:22 +0900

    [UPDATE] #BTS-140 : 오류 수정 -conflict 수정 때 잘못 지운 괄호때문에 나타나는 오류를 수정함.

--- pages/pointCharge.tsx
+++ pages/pointCharge.tsx
@@ -11,23 +11,7 @@ export default function PointCharge() {
   const router = useRouter();
   useEffect(() => {
     if (!cookies.accessToken) {
-      router.push("/login");
-      // Swal.fire({
-      //   title: "로그인 후 이용가능한 서비스 입니다.",
-      //   text: "로그인 페이지로 이동",
-      //   icon: "warning",
-      //   showCancelButton: true,
-      //   confirmButtonColor: "#6E48EB",
-      //   cancelButtonColor: "#7F8088",
-      //   confirmButtonText: "이동",
-      //   cancelButtonText: "취소",
-      // }).then((result) => {
-      //   if (result.isConfirmed) {
-      //     router.push("/login");
-      //   } else {
-      //     router.back();
-      //   }
-      // });
+      //router.push("/login");
     }
   }, []);
 
