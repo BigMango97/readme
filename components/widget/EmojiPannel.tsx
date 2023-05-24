8afa5804e918d46c559e715810768d4d8e2c8b23
BigMango97 <mgk04222@naver.com>
2023-05-24 21:11:37 +0900

    [UPDATE] #BTS-135 : 뷰어페이지
    - 마우스 이벤트에서 터치 이벤트로 변경하고나니 오류 발생(이모지선택, 판넬 상태 등)

--- components/widget/EmojiPannel.tsx
+++ components/widget/EmojiPannel.tsx
@@ -27,38 +27,17 @@ export default function EmojiPannel(props: EmojiPannelProps) {
   const [isPanelVisible, setIsPanelVisible] = useState(true);
   const panelRef = useRef<HTMLDivElement>(null);
 
-  const handleMove = (event: MouseEvent | TouchEvent) => {
+  const handleMove = (event: TouchEvent) => {
     event.preventDefault();
-    if (event instanceof MouseEvent) {
-      setPosition({ x: event.clientX, y: event.clientY });
-    } else {
-      setPosition({ x: event.touches[0].clientX, y: event.touches[0].clientY });
-    }
   };
 
   useEffect(() => {
-    window.addEventListener("mousemove", handleMove as any);
     window.addEventListener("touchmove", handleMove as any);
 
     return () => {
-      window.removeEventListener("mousemove", handleMove as any);
       window.removeEventListener("touchmove", handleMove as any);
     };
   }, []);
-
-  const handleClick = (event: MouseEvent | TouchEvent) => {
-    event.preventDefault();
-    if (panelRef.current) {
-      const rect = panelRef.current.getBoundingClientRect();
-      const offsetX = rect.width / 2;
-      const offsetY = rect.height / 2;
-      if (event instanceof MouseEvent) {
-        setPosition({ x: event.clientX - offsetX, y: event.clientY - offsetY });
-      } else {
-        setPosition({ x: event.touches[0].clientX - offsetX, y: event.touches[0].clientY - offsetY });
-      }
-    }
-  };
 
   useEffect(() => {
     const handleRender = () => {
@@ -75,6 +54,17 @@ export default function EmojiPannel(props: EmojiPannelProps) {
     handleRender();
   }, []);
 
+  useEffect(() => {
+    setIsPanelVisible(true);
+    const timer = setTimeout(() => {
+      setIsPanelVisible(false);
+    }, 100);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div
       ref={panelRef}
@@ -87,7 +77,6 @@ export default function EmojiPannel(props: EmojiPannelProps) {
         left: `${position.x}px`,
         top: `${position.y}px`,
       }}
-    
     >
       {data.map((item) => (
         <div
