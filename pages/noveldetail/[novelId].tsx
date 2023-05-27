e3642f6aa02c507e33f8a4dd9a7d11021552f8cd
BigMango97 <mgk04222@naver.com>
2023-05-27 11:39:07 +0900

    [UPDATE] #BTS-106 : 소설상세페이지
    - 리액트쿼리로 데이터 패칭 및 캐싱처리

--- pages/noveldetail/[novelId].tsx
+++ pages/noveldetail/[novelId].tsx
@@ -1,56 +1,58 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import NovelDatailHeader from "@/components/pages/noveldetail/NovelDatailHeader";
 import NovelDetailInfo from "@/components/pages/noveldetail/NovelDetailInfo";
 import NovelDetailMenu from "@/components/pages/noveldetail/NovelDetailMenu";
 import DetailFooter from "@/components/layouts/DetailFooter";
 import NovelTages from "@/components/pages/noveldetail/NovelTages";
-import { allDetailDatatype } from "@/types/model/mainDataType";
+
 import Config from "@/configs/config.export";
+import { useQuery } from "react-query";
+
 export default function Novel() {
-  const [data, setData] = useState<allDetailDatatype>();
   const router = useRouter();
   const [novelId, setnovelId] = useState(Number(router.query.novelId));
-  const baseUrl = Config().baseUrl;
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await axios.get(
-          `${baseUrl}/sections-service/v1/cards/novels/${novelId}`
-        );
-        setData(res.data.data);
-      } catch (error) {
-        console.log(error);
-      }
+
+  const novelbyIdData = async () => {
+    const baseUrl = Config().baseUrl;
+    const response = await axios.get(
+      `${baseUrl}/sections-service/v1/cards/novels/${novelId}`
+    );
+    return response.data;
+  };
+  const novelbyIdDataQuery = useQuery(
+    ["novelbyIdData", novelbyIdData, "novelId", novelId],
+    novelbyIdData,
+    {
+      staleTime: 5 * 60 * 1000,
+      cacheTime: 10 * 60 * 1000, // 5분 (밀리초 단위)
     }
-    if (novelId) {
-      fetchData();
-    }
-  }, [baseUrl,novelId]);
+  );
+  const novelbyIdDataResult = novelbyIdDataQuery?.data?.data;
 
   return (
     <>
-      {data && (
+      {novelbyIdDataResult && (
         <>
           <NovelDatailHeader
-            title={data.title}
-            author={data.author}
-            genre={data.genre}
-            serializationStatus={data.serializationStatus}
-            serializationDays={data.serializationDays}
+            title={novelbyIdDataResult.title}
+            author={novelbyIdDataResult.author}
+            genre={novelbyIdDataResult.genre}
+            serializationStatus={novelbyIdDataResult.serializationStatus}
+            serializationDays={novelbyIdDataResult.serializationDays}
           />
           <NovelDetailInfo
-            views={data.views}
-            starRating={data.starRating}
-            episodeCount={data.episodeCount}
-            thumbnail={data.thumbnail}
+            views={novelbyIdDataResult.views}
+            starRating={novelbyIdDataResult.starRating}
+            episodeCount={novelbyIdDataResult.episodeCount}
+            thumbnail={novelbyIdDataResult.thumbnail}
           />
-          <NovelTages tags={data.tags} />
+          <NovelTages tags={novelbyIdDataResult.tags} />
           <NovelDetailMenu
             novelId={novelId}
-            description={data.description}
-            authorComment={data.authorComment}
+            description={novelbyIdDataResult.description}
+            authorComment={novelbyIdDataResult.authorComment}
           />
         </>
       )}
