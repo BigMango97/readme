4b3427b214c6753f94d1855b9eece3e19c9dda73
sihyun3109 <sihyun3109@gmail.com>
2023-05-30 15:31:36 +0900

    [UPDATE] #BTS-140 : 결제 퍼블 수정 -결제 버튼 추가

--- components/pages/point/PointBottom.tsx
+++ components/pages/point/PointBottom.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "@/components/pages/point/PointBottom.module.css";
 import LineSeparator from "@/components/ui/LineSeparator";
 import { listAmountData } from "@/data/amountData";
 import axios from "@/configs/axiosConfig";
+import Image from "next/image";
+import { useRecoilState } from "recoil";
+import { payState } from "@/state/payState";
 
 export default function PointBottom() {
   //const uuid = localStorage.getItem("uuid");
 
-  const clickMoney = (point: number) => {
+  const clickMoney = () => {
     axios
       .post(`/payments-service/v1/payments/ready`, {
         point: point,
@@ -21,23 +24,34 @@ export default function PointBottom() {
         window.open(res.data.data.next_redirect_pc_url);
       });
   };
+  // const [cash, setCash] = useState(15000);
+  const [point, setPoint] = useRecoilState(payState);
+  //const clickButton = (id: number, amount: number) => {
+  //setClickAmount(id);
+  //setCash(amount);
+  //};
   return (
     <>
       <div className={style.container}>
         <div className={style.box}>
-          {" "}
           {listAmountData.map((item) => (
             <div key={item.id}>
-              <div
-                className={style.text}
-                onClick={() => clickMoney(item.point)}
-              >
+              <div className={style.text} onClick={() => setPoint(item.point)}>
                 <p>P {item.point}</p>
                 <span>{item.amount.toLocaleString("en")}</span>
               </div>
               <LineSeparator colorline="grayline" />
             </div>
           ))}
+        </div>
+        <div className={style.payment}>
+          <Image
+            src="/assets/images/kakaopayBtn.png"
+            alt="pay"
+            width={115}
+            height={45}
+            onClick={() => clickMoney()}
+          />
         </div>
       </div>
     </>
--- components/pages/point/PointMiddle.tsx
+++ components/pages/point/PointMiddle.tsx
@@ -2,24 +2,27 @@ import React, { useState } from "react";
 import style from "@/components/pages/point/PointMiddle.module.css";
 import LineSeparator from "@/components/ui/LineSeparator";
 import { buttonAmountData } from "@/data/amountData";
+import { useRecoilState } from "recoil";
+import { payState } from "@/state/payState";
 
 export default function PointMiddle() {
   const [clickAmount, setClickAmount] = useState(2);
-  const [cash, setCash] = useState(15000);
-  const clickButton = (id: number, amount: number) => {
-    setClickAmount(id);
-    setCash(amount);
-  };
+  //const [cash, setCash] = useState(15000);
+  const [point, setPoint] = useRecoilState(payState);
+  // const clickButton = (id: number, amount: number) => {
+  //   setClickAmount(id);
+  //   setCash(amount);
+  // };
   return (
     <>
       <div className={style.container}>
         <div className={style.amount}>
           <LineSeparator colorline="grayline" />
           <p>충전 캐시 금액을 클릭하세요</p>
-          <span>{cash.toLocaleString("en")}</span>
+          <span>{point.toLocaleString("en")}</span>
           <LineSeparator colorline="grayline" />
         </div>
-        <div className={style.bottom}>
+        {/* <div className={style.bottom}>
           {buttonAmountData.map((item) => (
             <div
               key={item.id}
@@ -29,7 +32,7 @@ export default function PointMiddle() {
               <p>P {item.amount.toLocaleString("en")}</p>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </>
   );
