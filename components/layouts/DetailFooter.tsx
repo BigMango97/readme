49cb78a42df7da48a602e6bf9c1b6bbdf3b88c37
sihyun3109 <sihyun3109@gmail.com>
2023-05-30 14:00:22 +0900

    [UPDATE] #BTS-150 : 좋아요 -기능 수정중.

--- components/layouts/DetailFooter.tsx
+++ components/layouts/DetailFooter.tsx
@@ -1,23 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "@/components/layouts/DetailFooter.module.css";
 import Image from "next/image";
 import isLogin from "@/configs/isLogin";
 import { useRouter } from "next/router";
 import Login from "@/pages/login";
 import { useCookies } from "react-cookie";
+import axios from "@/configs/axiosConfig";
+import { likeListType, likeType } from "@/types/user/likeType";
 export default function DetailFooter() {
   const [clickLike, setClickLike] = useState<boolean>(false);
+  const [cookies] = useCookies(["accessToken", "uuid"]);
+  const [userLikeList, setUserLikeList] = useState<likeListType>({
+    likeList: [],
+  });
+  const [loginCheck, setLoginCheck] = useState<boolean>(false);
   const router = useRouter();
-  const [cookies] = useCookies(["uuid"]);
+  const novelId = router.query.novelId;
+  // useEffect(() => {
+  //   setLoginCheck(cookies.accessToken);
+  // }, []);
+  useEffect(() => {
+    setLoginCheck(cookies.accessToken);
+    axios
+      .get(`/utils-service/v1/pick`, {
+        headers: {
+          uuid: `${cookies.uuid}`,
+        },
+      })
+      .then((res) => {
+        setUserLikeList({ likeList: res.data.data.contents });
+        // userLikeList.likeList.map((item) => {
+        //   if (item.novelsId === novelId) {
+        //     return setClickLike(true);
+        //   }
+        // });
+      });
+  }, []);
 
   const likeBtnHandle = () => {
     setClickLike(!clickLike);
+    axios
+      .post(
+        `/utils-service/v1/pick`,
+        {
+          novelsId: `${novelId}`,
+        },
+        {
+          headers: {
+            uuid: `${cookies.uuid}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      });
   };
-  //api불러와서 찜 목록 어쩌구 해...
+
   return (
     <div className={style.detailFooter}>
       <div className={style.novelBuyBtn}>
-        {isLogin() ? (
+        {loginCheck ? (
           <Image
             src={
               clickLike
