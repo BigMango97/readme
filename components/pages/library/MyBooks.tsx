fe5def2762305fe03dc40d160da63866e6720b94
sihyun3109 <sihyun3109@gmail.com>
2023-05-31 13:50:45 +0900

    [UPDATE] #BTS-131 : 빌드 오류 수정 빌드 시 일어나는 오류를 수정함.

--- components/pages/library/MyBooks.tsx
+++ components/pages/library/MyBooks.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "@/configs/axiosConfig";
 import { likeListType } from "@/types/user/likeType";
-import {
-  allDetailDataListType,
-  allDetailDatatype,
-} from "@/types/model/mainDataType";
+import { allDetailDatatype } from "@/types/model/mainDataType";
 import NovelCardList from "./NovelCardList";
 import { useCookies } from "react-cookie";
 import { useRouter } from "next/router";
@@ -14,7 +11,7 @@ export default function MyBooks() {
     likeList: [],
   });
   //const [likeNovelData, setLikeNovelData] = useState<allDetailDatatype[]>([]);
-  const [likeNovelData, setLikeNovelData] = useState<allDetailDataListType>();
+  //const [likeNovelData, setLikeNovelData] = useState<allDetailDataListType>();
   const [cookies] = useCookies(["uuid"]);
   let dataList: allDetailDatatype[] = [];
   useEffect(() => {
@@ -37,9 +34,9 @@ export default function MyBooks() {
           .then((res) => {
             dataList.push(res.data.data);
             console.log("res.data.data", res.data.data);
-            setLikeNovelData({
-              allDetailDataList: [...dataList, res.data.data],
-            });
+            // setLikeNovelData({
+            //   allDetailDataList: [...dataList, res.data.data],
+            // });
           })
           .catch((error) => {
             console.log(error);
@@ -49,12 +46,12 @@ export default function MyBooks() {
   //console.log("likeNovelData", likeNovelData);
   return (
     <>
-      <NovelCardList
+      {/* <NovelCardList
         data={likeNovelData}
         totalElements={
           likeNovelData ? likeNovelData.allDetailDataList.length : 0
         }
-      />
+      /> */}
     </>
   );
 }
