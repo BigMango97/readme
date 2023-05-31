09d51fe3fd3d58b232a70c6a0ff8e33e790b1419
sihyun3109 <sihyun3109@gmail.com>
2023-05-31 14:17:15 +0900

    [UPDATE] #BTS-140 : 로그인이 필요한 화면 수정 내 서재와 마이페이지에 들어갈 때 로그인 확인 여부를 체크하는 기능 수정.

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "@/components/layouts/Footer";
 import Logout from "@/components/pages/mypage/Logout";
 import MypageInfo from "@/components/pages/mypage/MypageInfo";
 import MypageList from "@/components/pages/mypage/MypageList";
 import isLogin from "@/configs/isLogin";
 import Login from "./login";
+import { useCookies } from "react-cookie";
 
 export default function Mypage() {
+  const [cookies] = useCookies(["accessToken"]);
+  const [loginCheck, setLoginCheck] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoginCheck(cookies.accessToken);
+  }, []);
   return (
     <>
-      <MypageInfo />
-      <MypageList />
-      <Logout />
-      <Footer />
+      {loginCheck ? (
+        <>
+          <MypageInfo />
+          <MypageList />
+          <Logout />
+          <Footer />
+        </>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
