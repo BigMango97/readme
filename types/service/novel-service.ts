09fb9aba60c95163652dc2726cf968f395aaf0bb
BigMango97 <mgk04222@naver.com>
2023-06-04 23:35:35 +0900

    [UPDATE] #BTS-135 : 뷰어페이지 - 읽은페이지 저장
    - 현재 읽은 페이지의 스크롤을 로컬스토리지에 저장 후 post요청
    - 뷰어페이지 관련 데이터 fetch 함수분리

--- types/service/novel-service.ts
+++ types/service/novel-service.ts
@@ -4,3 +4,20 @@ export interface besteventIdDataType {
     mainImage: string;
   };
 }
+
+//뷰어페이지 타입
+export interface episodeDetailFetchType {
+  data: {
+    content: string;
+    id: number;
+    novelId: number;
+    novelsTitle: string;
+    registration: string;
+    title: string;
+  };
+}
+export interface ViewerPositionData {
+    readAt: number | null;
+    episodeId: number;
+  }
+  
