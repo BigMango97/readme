75f7641ddae938f896e2879e0f228390940e80a0
sihyun3109 <sihyun3109@gmail.com>
2023-06-04 23:39:40 +0900

    [UPDATE] #BTS-161 : 유저 이미지 추가 -로그인 시 넘어오는 유저 프로필을 마이페이지에서 볼 수 있게 추가함.

--- components/pages/mypage/MypageInfo.tsx
+++ components/pages/mypage/MypageInfo.tsx
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from "react";
 import style from "@/components/pages/mypage/MypageInfo.module.css";
+import Image from "next/image";
 export default function MypageInfo() {
-  const [userName, setUserName] = useState<string>();
-  const [userPoint, setUserPoint] = useState<number>();
+  const [userName, setUserName] = useState<string>("");
+  const [userPoint, setUserPoint] = useState<number>(0);
+  const [userProfileImg, setUserProfileImg] = useState<string>("");
   useEffect(() => {
-    const name = localStorage.getItem("nickname") || undefined;
+    const name = localStorage.getItem("nickname") || "";
     setUserName(name);
-    const point = localStorage.getItem("point") || undefined;
+    const point = localStorage.getItem("point");
     setUserPoint(Number(point));
+
+    const profileImg = localStorage.getItem("profileImg") || "";
+    setUserProfileImg(profileImg);
   }, []);
   return (
     <>
       <div className={style.mypageContainer}>
         <div className={style.mypageContainerInfo}>
-          <p>
-            {userName}님 <br /> 오늘도 응원해요!
-          </p>
+          <div className={style.mypageTop}>
+            <Image
+              src={userProfileImg}
+              alt="profileImg"
+              width={120}
+              height={120}
+              priority
+            />
+            <p>
+              {userName}님 <br /> 오늘도 응원해요!
+            </p>
+          </div>
           <div className={style.mypageContainerPointInfo}>
             <div className={style.mypageContainerPoint}>
               포인트 : {userPoint?.toLocaleString("en")}P
--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require("next-pwa/cache")
+const runtimeCaching = require("next-pwa/cache");
 
 const withPWA = require("next-pwa")({
   dest: "public",
@@ -7,15 +7,19 @@ const withPWA = require("next-pwa")({
   skipWaiting: true,
   runtimeCaching,
   buildExcludes: [/middleware-manifest.json$/],
-  mode: 'production', 
+  mode: "production",
   disableDevLogs: true, //log닫기
 });
 
 const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
-    domains: ["btsimagebucket.s3.ap-northeast-2.amazonaws.com","dn-img-page.kakao.com"],
+    domains: [
+      "btsimagebucket.s3.ap-northeast-2.amazonaws.com",
+      "dn-img-page.kakao.com",
+      "k.kakaocdn.net",
+    ],
   },
-})
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
