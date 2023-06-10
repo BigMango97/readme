773bae3ac6f29f402a1ea81a3479f72428d2087b
Jason Ahn <beat1103@gmail.com>
2023-06-10 19:43:03 +0900

    feat: sidebar menu fixed

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -2,12 +2,29 @@ import Image from "next/image";
 import React from "react";
 import style from "@/components/layouts/Header.module.css";
 import MenuSlide from "@/components/layouts/MenuSlide";
+import SlidingPane from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
 import { useState } from "react";
 export default function Header() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   
   return (
     <header className={style.mainHeader}>
+      <SlidingPane
+        className="some-custom-class"
+        overlayClassName="some-custom-overlay-class"
+        from ='right'
+        width='85%'
+        isOpen={isOpen}
+        hideHeader={true}
+        onRequestClose={() => {
+          setIsOpen(false);
+        }}
+      >
+        <>
+        <MenuSlide isOpen={isOpen} setIsOpen={setIsOpen} />
+        </>
+      </SlidingPane>
       <div className={style.logo}>
         <h1>readme</h1>
         <Image
@@ -27,7 +44,6 @@ export default function Header() {
           priority
         />
       </div>
-      <MenuSlide isOpen={isOpen} setIsOpen={setIsOpen} />
     </header>
   );
 }
